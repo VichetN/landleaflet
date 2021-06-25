f755d39f6d28b70e12e36bfb2ca6fa55992a12f7
VichetN <vichet.noeng66@gmail.com>
2021-06-25 11:49:15 +0700

    Updates

---
+++ precache-manifest.c0e842441bc17becb56b52bb444d9974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cd96e4548deb90c7d3f81fabde041d",
+    "url": "/landleaflet/index.html"
+  },
+  {
+    "revision": "09a5f0105aa69a94843f",
+    "url": "/landleaflet/static/css/main.6c672271.chunk.css"
+  },
+  {
+    "revision": "575c34f399d280248c69",
+    "url": "/landleaflet/static/js/2.5df83daa.chunk.js"
+  },
+  {
+    "revision": "278be41389e3536a5347a9018737729d",
+    "url": "/landleaflet/static/js/2.5df83daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a5f0105aa69a94843f",
+    "url": "/landleaflet/static/js/main.d63cb22d.chunk.js"
+  },
+  {
+    "revision": "c3683813a7e02dbc0366",
+    "url": "/landleaflet/static/js/runtime-main.6f8b7c0d.js"
+  },
+  {
+    "revision": "8b8e805f363960265540b85d674cb339",
+    "url": "/landleaflet/static/media/spritesheet.8b8e805f.svg"
+  }
+]);
